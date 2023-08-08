db3009d8383f5fc7e481bae8a2e236f8a9b741e2
bhavesh-appbrew <bhavesh@appbrew.tech>
2023-08-08 23:02:31 +0530

    feat: first class, Intro to JS

---
+++ JavaScript-Lecture-1/script-linking.js
@@ -0,0 +1,54 @@
+// // Printing statement
+
+// console.log("hello world");
+// console.log(2);
+
+// // Declaration of variable
+
+// var xyz;
+
+// console.log(xyz)
+
+// // Initialization
+
+// xyz = 12;
+
+// // Declaration + Initilization
+
+// var abc = 14;
+
+// // another similar statement
+// // reInitialization
+
+// abc = 15;
+
+
+// //redeclaration
+// var abc;
+
+// console.log(xyz)
+
+// console.log(abc)
+
+
+// redeclaration is not possible with let keyword
+// let name = "bhavesh"
+// name = "mustafa"
+// let name = "vaishnavi"
+
+// redeclaration and reinitialization not possible with const
+
+// const familyName = "bansal"
+
+// const familyName = "singla"
+
+// let arr = [1,2, true, "abcd", "xyz", 1.1, 53495893757]
+// console.log(arr)
+
+let roleNumberStudent = {
+    "R1" : "Bhushan",
+    "R2" : "Manoj",
+    "R3" : "Mukesh"
+}
+
+console.log(roleNumberStudent)
