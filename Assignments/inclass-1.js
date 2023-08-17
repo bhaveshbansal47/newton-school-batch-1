887755b2624adbd29f616e638d79e6532bafdf2b
bhavesh-appbrew <bhavesh@appbrew.tech>
2023-08-17 23:02:27 +0530

    featL js lecture 2

---
+++ Assignments/inclass-1.js
@@ -0,0 +1,10 @@
+const car = {
+    color: "white",
+    seats: 2,
+    nitros: false
+}
+
+// const msg = "My car have " + car.seats + " seats and it is of color " + car.color
+const msg = `My car have ${car.seats} seat and it is of color ${car.color}`
+
+console.log(msg)
---
+++ JavaScript-Lecture-2/functions.js
@@ -0,0 +1,16 @@
+// function print(value) {
+//     console.log(value)
+// }
+
+// print("hello")
+
+function recurse(val) {
+    if(val === 5) {
+        return
+    }
+    console.log(val)
+    val = val + 1
+    recurse(val)
+}
+
+recurse(1)
