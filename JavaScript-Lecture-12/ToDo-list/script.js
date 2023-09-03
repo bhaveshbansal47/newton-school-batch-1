9806537b2a67a8060b2a7622838dea809581be75
bhavesh-appbrew <bhavesh@appbrew.tech>
2023-09-03 22:02:33 +0530

    js lecture 12 part -2

--- JavaScript-Lecture-12/ToDo-list/script.js
+++ JavaScript-Lecture-12/ToDo-list/script.js
@@ -12,11 +12,11 @@ function addTask(event) {
     // create complete button
     const comleteButton = document.createElement('button')
     comleteButton.innerText = 'Complete'
-    comleteButton.setAttribute('class','complete-btn')
+    comleteButton.setAttribute('class', 'complete-btn')
     // create delete button
     const deleteButton = document.createElement('button')
     deleteButton.innerText = 'Delete'
-    deleteButton.setAttribute('class','trash-btn')
+    deleteButton.setAttribute('class', 'trash-btn')
     // push li and buttons inside div
     divNode.appendChild(listNode)
     divNode.appendChild(comleteButton)
@@ -24,7 +24,27 @@ function addTask(event) {
     // fetch ul and append the div node
     const ulNode = document.getElementsByClassName('todo-list')[0]
     ulNode.appendChild(divNode)
+    // add complete functionality
+    addCompleteFunctionality(comleteButton, listNode)
+    // add delete functionality
+    addDeleteFunctionality(deleteButton, divNode)
 }
 
 const addButton = document.getElementsByClassName('todo-button')[0]
 addButton.addEventListener('click', addTask)
+
+function addCompleteFunctionality(completeButton, listNode) {
+    completeButton.addEventListener('click', function () {
+        if (listNode.style['text-decoration'] !== '') {
+            listNode.style['text-decoration'] = ''
+        } else {
+            listNode.style['text-decoration'] = 'line-through solid rgb(0, 0, 0)'
+        }
+    })
+}
+
+function addDeleteFunctionality(deleteButton, divNode) {
+    deleteButton.addEventListener('click', function () {
+        divNode.remove()
+    })
+}
