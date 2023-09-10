02e701faca80546044001f72b00f60a70dc03387
bhavesh-appbrew <bhavesh@appbrew.tech>
2023-09-10 21:53:19 +0530

    js laecture 16 q1 contest

---
+++ JavaScript Lecture-16/forntend-bollywood/script.js
@@ -0,0 +1,25 @@
+const actualWord = "actor"
+const letterNodes = document.getElementsByClassName('letter')
+let currentPosition = 0
+let guesses = 5
+
+document.body.addEventListener('keydown', function (event) {
+    const currentLetter = event.key
+    if (actualWord[currentPosition] === currentLetter) {
+        letterNodes[currentPosition].innerText = currentLetter
+        currentPosition += 1
+        if (currentPosition === 5) {
+            alert("Congratulations!")
+        }
+    } else {
+        guesses -= 1
+        document.getElementById('guesses').innerText = `Chances Left : ${guesses}`
+        if (guesses === 0) {
+            alert("You've run out of guesses! Game over!")
+            document.getElementById('answer').innerText = `Answer is ${actualWord}`
+        } else {
+            alert(`Wrong guess! ${guesses} guesses remaining.`)
+        }
+
+    }
+})
